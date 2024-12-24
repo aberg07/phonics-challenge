407abf6b5d5b6b7d663b76d64b250627fb16a941
aberg07 <140882154+aberg07@users.noreply.github.com>
2024-12-24 13:52:18 +0900

    fix: responsive design for mobile phone screens now implemented across the whole site

--- components/SampleCard.vue
+++ components/SampleCard.vue
@@ -39,7 +39,7 @@
             <source :src="getSoundPath" type="audio/mpeg">
         </audio>
         <div class="flex flex-col items-center justify-center">
-            <img class="w-12 md:w-16" src="/_nuxt/assets/icons/volume-icon.png">
+            <img class="w-12 md:w-16" src="icons/volume-icon.png">
             <!-- If the phonic sound introduced is at the start of the word, simply color the phonic sound red and the rest in black -->
             <p v-if="getHighlightIndex==0" class="text-4xl md:text-3xl"><span class="text-red-500">{{ props.word.substring(getHighlightIndex, getHighlightIndex+props.phonicSound.length) }}</span><span>{{ props.word.substring(1) }}</span></p>
             <!-- Otherwise color letters in black up until the phonic sound occurs, where it will be colored red. Any letters that occur afterward will be in black -->
