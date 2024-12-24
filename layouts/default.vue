690f019345096220f17976ba3630cb92496af49f
aberg07 <140882154+aberg07@users.noreply.github.com>
2024-12-24 13:52:18 +0900

    fix: responsive design for mobile phone screens now implemented across the whole site

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-orange-300 h-screen">
         <div class="bg-sky-300 flex flex-col justify-center place-items-start border-gray-600 border-b-2">
-            <a class="ml-4" href="/"><img class="w-56" src="~/assets/images/logos/phonics challenge logo.png"></a>
+            <a class="ml-4" href="/"><img class="w-56 lg:w-56 md:w-72" src="~/assets/images/logos/phonics challenge logo.png"></a>
         </div>
         <slot></slot>
     </div>
