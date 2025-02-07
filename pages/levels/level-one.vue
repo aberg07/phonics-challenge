976dc83ea7b7819cafc5d7a81ebc9203666d87d7
aberg07 <140882154+aberg07@users.noreply.github.com>
2025-02-07 11:59:58 +0900

    chore: updated dependencies

--- pages/levels/level-one.vue
+++ pages/levels/level-one.vue
@@ -5,15 +5,15 @@
         soundPath: string
     }
     const levelOneWords: wordCard[] = [
-        {word: "In", imgPath: '/images/in-with-arrow.png', soundPath:'/sounds/levels/level-one/in.mp3'},
-        {word: "Tap", imgPath: '/images/tap.png', soundPath:'/sounds/levels/level-one/tap.mp3'},
-        {word: "Nap", imgPath: '/images/nap.png', soundPath:'/sounds/levels/level-one/nap.mp3'},
-        {word: "Pan", imgPath: '/images/pan.png', soundPath:'/sounds/levels/level-one/pan.mp3'},
-        {word: "Pat", imgPath: '/images/pat.png', soundPath:'/sounds/levels/level-one/pat.mp3'},
-        {word: "Pin", imgPath: '/images/pin.png', soundPath:'/sounds/levels/level-one/pin.mp3'},
-        {word: "Pit", imgPath: '/images/pit.png', soundPath:'/sounds/levels/level-one/pit.mp3'},
-        {word: "Snap", imgPath: '/images/snap.png', soundPath:'/sounds/levels/level-one/snap.mp3'},
-        {word: "Pants", imgPath: '/images/pants.png', soundPath:'/sounds/levels/level-one/pants.mp3'}
+        {word: "in", imgPath: '/_nuxt/assets/images/in-with-arrow.png', soundPath:'/_nuxt/assets/sounds/levels/level-one/in.mp3'},
+        {word: "tap", imgPath: '/_nuxt/assets/images/tap.png', soundPath:'/_nuxt/assets/sounds/levels/level-one/tap.mp3'},
+        {word: "nap", imgPath: '/_nuxt/assets/images/nap.png', soundPath:'/_nuxt/assets/sounds/levels/level-one/nap.mp3'},
+        {word: "pan", imgPath: '/_nuxt/assets/images/pan.png', soundPath:'/_nuxt/assets/sounds/levels/level-one/pan.mp3'},
+        {word: "pat", imgPath: '/_nuxt/assets/images/pat.png', soundPath:'/_nuxt/assets/sounds/levels/level-one/pat.mp3'},
+        {word: "pin", imgPath: '/_nuxt/assets/images/pin.png', soundPath:'/_nuxt/assets/sounds/levels/level-one/pin.mp3'},
+        {word: "pit", imgPath: '/_nuxt/assets/images/pit.png', soundPath:'/_nuxt/assets/sounds/levels/level-one/pit.mp3'},
+        {word: "snap", imgPath: '/_nuxt/assets/images/snap.png', soundPath:'/_nuxt/assets/sounds/levels/level-one/snap.mp3'},
+        {word: "pants", imgPath: '/_nuxt/assets/images/pants.png', soundPath:'/_nuxt/assets/sounds/levels/level-one/pants.mp3'}
     ];
 </script>
 
